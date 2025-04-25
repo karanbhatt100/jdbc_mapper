5edd7a271fb7b3c9f8487d55c3973e04ec95f7f1
karanbhatt100 <karanbhatt100@gmail.com>
2025-04-25 14:41:52 +0530

    most chart done

--- GraphChart.py
+++ GraphChart.py
@@ -1,54 +1,58 @@
 import os
+from collections import OrderedDict
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import mplcursors
 import numpy as np
+import math
 
 csv_directory = 'D:\\Projects\\jdbc_mapper\\cycleLog'
 read_col = 'timeInNano'
+list_size = 'listSize'
 
-def array_ele(ele_array, dict_by_ele_size):
-    for i in ele_array:
-        if i['listSize'][0] in dict_by_ele_size:
-            dict_by_ele_size[i['listSize'][0]].append(i[read_col][0])
-        else:
-            dict_by_ele_size[i['listSize'][0]] = [i[read_col][0]]
-    return
 
 def print_average(dict_by_ele_size):
-    for k,v in dict_by_ele_size.items():
-        print(f"Average time to create list of size {k} is {np.average(v)}")
-    return
+    _dict = {}
+    for k, v in dict_by_ele_size.items():
+        print(f"Average time to create list of size {k} is {np.average(v)} in {read_col}")
+        _dict[k] = math.floor(np.average(v))
+    return _dict
+
 
 def get_data():
-    list_elements = []
-    all_data = []
-    dict_by_ele_size = {}
+    csv_dataframe = []
+    size_element = []
+    res = {}
 
     for filename in os.listdir(csv_directory):
         if filename.endswith('.csv'):
             file_path = os.path.join(csv_directory, filename)
 
-            data = pd.read_csv(file_path,usecols=['listSize', read_col])
+            data = pd.read_csv(file_path, usecols=[list_size, read_col])
+            for i in data[list_size]:
+                size_element.append(f"{i}")
 
-            for i in data['listSize']:
-                list_elements.append(f"{i}")
+            csv_dataframe.append(data)
 
-            all_data.append(data[['listSize', read_col]])
-            array_ele(all_data, dict_by_ele_size)
+    size_element = set(size_element)
+    for size in size_element:
+        res.setdefault(size, [])
+        for data in csv_dataframe:
+            filtered_names = data.loc[data[list_size] == int(size), [read_col]].values.tolist()
+            res[size].append(filtered_names[0][0])
+    return res, size_element
 
-    print_average(dict_by_ele_size)
-    return all_data, list_elements
 
-all_data, list_elements = get_data()
-combined_data = pd.concat(all_data, ignore_index=False)
+combined_data, list_elements = get_data()
+average_size_dict = print_average(combined_data)
 
-plt.bar(list_elements, combined_data['timeInNano'], label='Data')
+plt.bar(average_size_dict.keys(), average_size_dict.values(), label='Data')
 
 plt.xlabel('Total List Size')
 plt.ylabel('Time In Nanosecond')
 plt.title('Line Chart from Multiple CSV Files')
-plt.xticks(list_elements)
 mplcursors.cursor(hover=True)
+#plt.xticks(list_elements)
 plt.legend()
 plt.show()
